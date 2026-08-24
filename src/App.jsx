29db0a7cb3548a567b3ec2de6da6a80a1e067af2
Munaza Munir <munazaengineering@gmail.com>
2026-08-24 17:16:33 +0500

    Fix component import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import About from "./Components/About";
-import Skills from "./Components/Skills";
-import Services from "./Components/Services";
-import Projects from "./Components/Projects";
-
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Journey from "./components/Journey";
 import "./App.css";
-import Journey from "./components/journey";
 
 function App() {
   return (
@@ -18,11 +16,11 @@ function App() {
       <Hero />
       <About />
       <Skills />
-      <Services/>
-      <Projects/>
-      <Journey/>
-      <Contact/>
-      <Footer/>
+      <Services />
+      <Projects />
+      <Journey />
+      <Contact />
+      <Footer />
     </>
   );
 }
